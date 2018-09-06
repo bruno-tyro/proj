a7648dbc2d3a3b4a4f38b014443d6122591ace61
bsilva-test <43026376+bsilva-test@users.noreply.github.com>
2018-09-06 16:42:30 +1000

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require("express");
 
  //Respond with "hello world" for requests that hit our root "/"
  app.get("/", function (req, res) {
-  res.send("hello world");
+  res.send("hello world! this is bruno test");
   });
 
   //listen to port 3000 by default
